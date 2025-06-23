e394bb19e4cc505a3dbe31d031d89948bb371bda
Fajarrr86 <adty.fjr4@gmail.com>
2025-06-23 22:40:18 +0700

    Aku nambahin dikit di bagian mission

--- src/main/java/com/starlight/controller/MissionController.java
+++ src/main/java/com/starlight/controller/MissionController.java
@@ -1,9 +1,25 @@
 package com.starlight.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class MissionController {
+public class MissionController implements Initializable {
+    @FXML
+    private Button missionButton;
+    @FXML
+    private Button streakButton;
+    @FXML
+    private Button dailyButton;
+    @FXML
+    private Button rewardButton;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
     
 }
