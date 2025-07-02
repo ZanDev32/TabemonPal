5ea5977a5f6c4d99f85855414290a503fc9634d7
Fajarrr86 <adty.fjr4@gmail.com>
2025-07-02 21:58:22 +0700

    p

--- src/main/java/com/starlight/controller/MissionController.java
+++ src/main/java/com/starlight/controller/MissionController.java
@@ -1,22 +1,10 @@
 package com.starlight.controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 public class MissionController implements Initializable {
-    @FXML
-    private Button missionButton;
-    @FXML
-    private Button streakButton;
-    @FXML
-    private Button dailyButton;
-    @FXML
-    private Button rewardButton;
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
