cd948269ae6fc4a886299c8a4b90c858048821e8
Gnockz11 <Rafiraditya642006@gmail.com>
2025-07-20 14:29:02 +0700

    Tabestreak FXML Create file

---
+++ src/main/java/com/starlight/controller/Tabestreak.java
@@ -0,0 +1,21 @@
+package com.starlight.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+/**
+ * Placeholder controller for the games view.
+ */
+public class Tabestreak implements Initializable {
+
+    /**
+     * Initializes the controller. Currently no-op.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // no-op
+    }
+
+}
